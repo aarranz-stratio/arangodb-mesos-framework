28795f400635d823ba93d32d8858c439fb78b831
Andreas Streichardt <andreas@arangodb.com>
2016-04-18 11:15:00 +0200

    Remove obsolete js init stuff

--- bin/discover.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo Starting discovery for agency at $1 ...
-arangosh --javascript.execute `dirname $0`/discover.js "$1"
--- bin/initAgency.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo Initializing agency at $1:$2 ...
-exec arangosh --javascript.execute `dirname $0`/initAgency.js "$1:$2"
