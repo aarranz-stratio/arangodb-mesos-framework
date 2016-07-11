ba73e1f97f2147b3a9149a842c2d68e31250b4cc
Andreas Streichardt <andreas@arangodb.com>
2016-07-11 14:53:16 +0200

    no more release lines

--- release.sh
+++ release.sh
@@ -1,21 +1,20 @@
 #/bin/sh
 
-if [ $# -lt  3 ]; then
-  echo "$0 <docker-image-name> <mesosphere-release-line> <build-tag>"
-  echo "Example: $0 arangodb/arangodb-mesos-framework mesosphere-V2 build7"
+if [ $# -lt 2 ]; then
+  echo "$0 <docker-image-name> <build-tag>"
+  echo "Example: $0 arangodb/arangodb-mesos-framework 3.0-build2"
   exit 99
 fi
 
 git remote get-url origin > mesos-scripts/BUILDTAG
 git rev-parse HEAD >> mesos-scripts/BUILDTAG
 echo $3 >> mesos-scripts/BUILDTAG
-git tag -s $3
+git tag -s $2
 
 docker build -t $1:$2 .
-docker tag $1:$2 $1:$3
 
-echo "Git tag created: $3"
-echo "Docker images created: $1:$2, $1:$3"
+echo "Git tag created: $2"
+echo "Docker image created: $1:$2"
 echo ""
 echo "To finish the release:"
-echo "docker push $1:$2 && docker push $1:$3 && git push --follow-tags"
+echo "docker push $1:$2 && git push --follow-tags"
